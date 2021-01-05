7594ac4845dbb8821a9f1b5c3f7a68bf51ad87a6
fan-haobai <fanhaobai@gmail.com>
2021-01-05 17:28:22 +0800

    feat:add composer update

--- dockerfiles/php/start.sh
+++ dockerfiles/php/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dir=/var/www/blog
+cd $dir/disqus
+
+# 更新composer
+composer update
+
 # 增加host.docker.internal
 ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
 
